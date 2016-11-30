5550f8a056f08b2ab9414ea80203b261a6f75684
Daniel <danassouline@hotmail.fr>
2016-11-30 17:05:31 +0100

    Modif script install mmonit

--- script/monitInstall.sh
+++ script/monitInstall.sh
@@ -1,3 +1,29 @@
 #! /bin/bash
 
+# Install M/Monit
+wget https://mmonit.com/dist/mmonit-3.6.2-linux-x64.tar.gz
+tar -xzvf mmonit-3.6.2-linux-x64.tar.gz
+rm mmonit-3.6.2-linux-x64.tar.gz
+cd mmonit-3.6.2
+
+# Start M/Monit
+./bin/mmonit start
+
+# Install Monit
 sudo apt-get install monit
+
+# Configure Monit
+sudo echo "set daemon 60
+
+set eventqueue basedir /var/monit slots 1000
+set mmonit http://admin:swordfish@localhost:8080/collector
+set mailserver localhost
+set alert root@localhost
+set logfile syslog facility log_daemon
+
+set httpd port 2812 and use address localhost
+allow localhost
+allow admin:monit" >> /etc/monit/monitrc
+
+# Start Monit
+sudo service monit start
