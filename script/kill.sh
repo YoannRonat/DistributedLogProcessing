15c0e204369498e2ad665c2c446ed926fe83c3a2
Tom RUSSELLO <tom.russello@ensimag.grenoble-inp.fr>
2016-12-07 00:54:17 +0100

    usage for kill.sh

--- script/kill.sh
+++ script/kill.sh
@@ -2,11 +2,14 @@
 VERT="\\033[1;32m"
 NORMAL="\\033[0;39m"
 
+if [[ $# -ne 2 ]]; then
+	echo "usage: ./kill.sh [machine id] [process name]" >&2
+	exit
+fi
 
 ################# Function definition ###################
 
 kill_process () {
-
 	# These variables allow us to use ssh or not depending on the server
 	cmd="ssh -i  ~/.ssh/xnet xnet@server-$1 \""
 	end_cmd="\""
