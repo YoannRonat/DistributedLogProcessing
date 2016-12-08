02ae4bf83195711db6031a147ef44b7f868d60df
Robichou <robin.teughels@gmail.com>
2016-12-08 18:16:35 +0100

    deploiement des scripts

---
+++ script/Recuperation_logs/deploiement_script.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# script de deploiement des scripts de telechargement
+# vers les serveurs
+
+scp script/logDl.sh xnet@server-1:
+scp script/logDl.sh xnet@server-2:
+scp script/logDl.sh xnet@server-3:
