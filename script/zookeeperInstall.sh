21c1caef0f176fca35d7ea3466faaf8b340d8b77
Robichou <robin.teughels@gmail.com>
2017-01-06 14:07:22 +0100

    petite correction de la parallèlisation

--- script/zookeeperInstall.sh
+++ script/zookeeperInstall.sh
@@ -34,7 +34,7 @@ zookeeper_configuration() {
 	# Zookeeper-master-worker compilation
 	# TODO : A mettre dans install.sh
 	echo -e "$VERT" "Zookeeper-master-worker installation on server-"$num"..." "$NORMAL"
-	eval ""$cmd"cd zookeeper-master-worker && sudo mvn install && cd"$end_cmd""
+	eval ""$cmd"cd zookeeper-master-worker && mvn install && cd"$end_cmd""
 	echo -e "$VERT" "Zookeeper-master-worker installation on server-"$num" [OK]" "$NORMAL"
 	
 	
@@ -56,7 +56,7 @@ zookeeper_start_master() {
 	eval ""$cmd"sudo sh $zooDir/bin/zkServer.sh"$end_cmd""
 	
 	echo -e "$VERT" "Lancement du Master-Worker sur server-"$num"..." "$NORMAL"
-	eval ""$cmd"java -cp .:/home/xnet/zookeeper/zookeeper-3.4.9.jar:/home/xnet/zookeeper/lib/slf4j-api-1.6.1.jar:/home/xnet/zookeeper/lib/slf4j-log4j12-1.6.1.jar:/home/xnet/zookeeper/lib/log4j-1.2.16.jar:/home/xnet/zookeeper-book-example/target/ZooKeeper-Book-0.0.1-SNAPSHOT.jar org.apache.zookeeper.book.Master server-1:2181"$end_cmd""
+	eval ""$cmd"cd zookeeper-master-worker && java -cp .:/home/xnet/zookeeper/zookeeper-3.4.9.jar:/home/xnet/zookeeper/lib/slf4j-api-1.6.1.jar:/home/xnet/zookeeper/lib/slf4j-log4j12-1.6.1.jar:/home/xnet/zookeeper/lib/log4j-1.2.16.jar:/home/xnet/zookeeper-master-worker/target/ZooKeeper-Book-0.0.1-SNAPSHOT.jar org.apache.zookeeper.book.Master server-1:2181"$end_cmd""
 	echo -e "$VERT" "Lancement du Master-Worker sur server-"$num" [OK]" "$NORMAL"
 }
 
@@ -69,14 +69,15 @@ zookeeper_start_worker() {
 	[[ $# > 0 ]] &&  cmd="ssh -i  ~/.ssh/xnet xnet@server-$1 \"" && end_cmd="\"" && num="$1"
 
 	echo -e "$VERT" "Lancement du Master-Worker sur server-"$num"..." "$NORMAL"
-	eval ""$cmd"java -cp .:/home/xnet/zookeeper/zookeeper-3.4.9.jar:/home/xnet/zookeeper/lib/slf4j-api-1.6.1.jar:/home/xnet/zookeeper/lib/slf4j-log4j12-1.6.1.jar:/home/xnet/zookeeper/lib/log4j-1.2.16.jar:/home/xnet/zookeeper-book-example/target/ZooKeeper-Book-0.0.1-SNAPSHOT.jar org.apache.zookeeper.book.Worker server-1:2181"$end_cmd""
+	eval ""$cmd"cd zookeeper-master-worker && java -cp .:/home/xnet/zookeeper/zookeeper-3.4.9.jar:/home/xnet/zookeeper/lib/slf4j-api-1.6.1.jar:/home/xnet/zookeeper/lib/slf4j-log4j12-1.6.1.jar:/home/xnet/zookeeper/lib/log4j-1.2.16.jar:/home/xnet/zookeeper-master-worker/target/ZooKeeper-Book-0.0.1-SNAPSHOT.jar org.apache.zookeeper.book.Worker server-1:2181"$end_cmd""
 	echo -e "$VERT" "Lancement du Master-Worker sur server-"$num" [OK]" "$NORMAL"
 }
 
 zookeeper_configuration "1" & zookeeper_configuration "2" & zookeeper_configuration "3" & wait
 
-zookeeper_start_master "1"
-zookeeper_start_worker "2" & zookeeper_start_worker "3" & wait
+zookeeper_start_master "1" &
+zookeeper_start_worker "2" &
+zookeeper_start_worker "3" & # wait
 
 # cd
 # ./zookeeper/bin/zkServer.sh
