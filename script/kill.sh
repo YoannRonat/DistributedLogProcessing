764095abd61fc841551a9f625473aed1103f5808
Tom RUSSELLO <tom.russello@ensimag.grenoble-inp.fr>
2017-01-19 00:19:57 +0100

    kill before stopping the process

--- script/kill.sh
+++ script/kill.sh
@@ -10,8 +10,8 @@ fi
 ################# Function definition ###################
 
 kill_process () {
-	ssh -i  ~/.ssh/xnet xnet@server-"$1" "sudo systemctl stop "$2".service > /dev/null"
 	ssh -i  ~/.ssh/xnet xnet@server-"$1" "ps ax | grep -E '"$2"' | awk -F ' ' '{print \$1}' | xargs sudo kill -9 > /dev/null"
+	ssh -i  ~/.ssh/xnet xnet@server-"$1" "sudo systemctl stop "$2".service > /dev/null"
 }
 
 kill_process "$1" "$2"
