fb704843d10e382fc2e54317f09350b73c88bf79
Yoann Ronat <yoann.ronat@ensimag.grenoble-inp.fr>
2017-01-18 15:56:21 +0100

    Éxecution du benchmark et récupération du résultat - Utilisation du client zookeeper pour executer le bench - Temps d'éxecution (float) enregistré dans /tmp/bench avant d'être   récupéré par le script de benchmark - Utilisation de perl pour effectuer les calculs sur les flottant au lieu   de bc (non installé par défaut

--- script/benchmark.sh
+++ script/benchmark.sh
@@ -16,7 +16,7 @@ echo -e "Nombre de logs injectés,Nombre de workers,Moyenne,Variance,Execution 1
 NB_EXEC=10
 declare -a NB_LOGS=(50 100 500 2000);
 declare -a NB_WORKERS=(1 3);
-result=0
+result=0.0
 
 #Pour tous les nombres de machines possibles
 for nb_worker in ${NB_WORKERS[@]}
@@ -27,28 +27,32 @@ do
 	echo "Execution avec $nb_worker worker et $nb_log logs."
 #TODO : Recompiler le client à chaque fois qu'on change le nombre de logs injectés (client.java ligne 311)
 		echo -e "$nb_log,$nb_worker,MOYENNE,VARIANCE,\c" >> "$CSV_FILE_NAME"
-		declare -a results=();
+		declare -a results=()
 		#On execute $NB_EXEC fois le benchmark
 		for ((i=0 ; $NB_EXEC-$i ; i++))
-		do 
-#TODO : Calculer le vrai résultat
-			result=$i
-			let "results[$i]=$result"
+		do
+			# Injecte les logs à partir du client zookeeper 
+			# Mesure le temps d'execution réel en secondes (float)
+			# Enregistre le résultat dans un fichier temporaire
+			# N'affiche pas le résultat de l'éxecution
+			/usr/bin/time -f "%e" -o /tmp/bench java -cp /home/xnet/ZooKeeper-Book.jar org.apache.zookeeper.book.Client server-2:2181,server-1:2181,server-3:2181
+			result=$(cat /tmp/bench)
+			results[$i]=$result
 			echo -e "$result,\c" >> "$CSV_FILE_NAME"
 		done
 		#Calcul de la moyenne
-		tot=0
+		tot=0.0
 		for result in ${results[@]}; do
-		  let "tot+=$result"
+		  tot=$(perl -e "print $result + $tot")
 		done
-		moyenne=$(echo "$tot/$NB_EXEC" | bc -l)
+		moyenne=$(perl -e "print $tot/$NB_EXEC")
 		
 		#Calcul de la variance
 		sum=0
 		for result in ${results[@]}; do
-		  sum=$(echo "$sum+($result-$moyenne)*($result-$moyenne)" | bc -l )
+		  sum=$(perl -e "print $sum+($result-$moyenne)*($result-$moyenne)")
 		done
-		variance=$(echo "$sum/$NB_EXEC" |bc -l)
+		variance=$(perl -e "print $sum/$NB_EXEC")
 
 		#On ajoute la moyenne et la variance dans le fichier Excel
 		sed -i -e "s/MOYENNE/$moyenne/g" "$CSV_FILE_NAME"
