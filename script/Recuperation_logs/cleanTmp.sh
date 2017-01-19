e38458f4c9da1a6c384a3658df63022246972c2b
Tom RUSSELLO <tom.russello@ensimag.grenoble-inp.fr>
2017-01-19 12:40:18 +0100

    Recuperation_Logs removed

--- script/Recuperation_logs/cleanTmp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Script qui nettoie les dossiers de log sur chaque machine
-
-ssh -i ~/.ssh/xnet xnet@server-1 "rm -r /tmp/logs_tf/" &
-ssh -i ~/.ssh/xnet xnet@server-2 "rm -r /tmp/logs_tf/" &
-ssh -i ~/.ssh/xnet xnet@server-3 "rm -r /tmp/logs_tf/" &
--- script/Recuperation_logs/deploiement_script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# script de deploiement des scripts de telechargement
-# vers les serveurs
-
-scp script/logDl.sh xnet@server-1:
-scp script/logDl.sh xnet@server-2:
-scp script/logDl.sh xnet@server-3:
