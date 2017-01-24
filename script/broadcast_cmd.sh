120c0cd33ac5e3864c084a68e3ee111704d0c04e
Tom RUSSELLO <tom.russello@grenoble-inp.org>
2017-01-24 21:17:26 +0100

    Update broadcast_cmd.sh

--- script/broadcast_cmd.sh
+++ script/broadcast_cmd.sh
@@ -12,10 +12,10 @@ fi
 
 
 broadcast() {
-	# Lancement du master
+	# Lancement de la commande sur 1 serveur
 	echo -e "$VERT" "Lancement de la commande sur server-"$1"..." "$NORMAL"
 	ssh -i  ~/.ssh/xnet xnet@server-"$1" "$2"
 	echo -e "$VERT" "Lancement de la commande sur server-"$1" [OK]" "$NORMAL"
 }
-
+# Lancement de la commande sur les 3 serveurs
 broadcast "1" "$1" & broadcast "2" "$1" & broadcast "3" "$1" & wait
